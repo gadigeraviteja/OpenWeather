2ba049ae01d6d9da4517be1c72cf6e336c756c1e
raviteja <gadige.raviteja@gmail.com>
2023-04-13 17:12:08 +0530

    initial setup

---
+++ OpenWeatherApp/OpenWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeatherApp
+//
+//  Created by G RaviTeja on 13/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
